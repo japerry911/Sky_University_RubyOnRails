033fd4a597deda9495677bdfdef851ea43ca5b3a
japerry911 <jack.e.perry@outlook.com>
2020-03-12 09:43:22 -0600

    Working on login logout, added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "courses/new", to: "courses#new"
   get "about", to: "pages#about"
   resources :students, only: [:index, :new, :create, :show, :edit, :update]
+  resources :login, only: [:new, :create]
+  delete "logout", to: "login#destroy"
 end
